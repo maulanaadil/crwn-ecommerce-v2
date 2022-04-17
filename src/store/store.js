7d9f3cf7729253d3cd398814600ac6ab75a70d43
Maulana Adil <maulanaadil.latif@gmail.com>
2022-04-17 13:36:11 +0700

    convert categoriesMap state into redux-saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,17 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+import createSagaMiddleware from "@redux-saga/core";
 
-import { rootReducer } from "./root-reducer";
+import { rootSaga } from "./root-saga";
+import { persistedReducer } from "./persist-config";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
@@ -32,5 +26,7 @@ export const store = createStore(
   undefined,
   composedEnchancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
