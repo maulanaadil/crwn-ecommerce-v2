cd113b8378f8afef10d122b3643cb93f5502dae5
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-04 16:52:35 +0700

    @feature/refactor category (index) into typescript

---
+++ src/components/category/index.tsx
@@ -0,0 +1,5 @@
+import Category from "./category-container";
+import CategoryItem from "./category-item";
+import CategoryPreview from "./category-preview";
+
+export { Category, CategoryItem, CategoryPreview };
