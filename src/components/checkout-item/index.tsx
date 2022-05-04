1bb2086cb600b4af04788aeccdd1d316ea258f2b
Maulana Adil <maulanaadil.latif@gmail.com>
2022-05-04 16:53:02 +0700

    @feature/refactor checkout item components into typescript

---
+++ src/components/checkout-item/index.tsx
@@ -0,0 +1,69 @@
+import { FC } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { selectCartItems } from "@Store/cart/cart-selector";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  clearItemFromCart,
+} from "@Store/cart/cart-action";
+
+import {
+  CheckoutItemContainer,
+  Arrow,
+  ImageContainer,
+  Image,
+  QuantityContainer,
+  TextName,
+  TextQuantity,
+  TextPrice,
+  RemoveButton,
+} from "./styles";
+import { CheckoutItemProps } from "./types";
+
+const CheckoutItem: FC<CheckoutItemProps> = ({ cartItem }) => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
+
+  const { name, price, imageUrl, quantity } = cartItem;
+
+  const clearItemHandler = () =>
+    dispatch(clearItemFromCart(cartItems, cartItem));
+
+  const increaseItemHandler = () =>
+    dispatch(addItemToCart(cartItems, cartItem));
+
+  const decreaseItemHandler = () =>
+    dispatch(removeItemFromCart(cartItems, cartItem));
+
+  const checkingQuantity = (quantity: number) => {
+    if (quantity === 1) {
+      return (
+        <Arrow style={{ color: "gray" }} onClick={decreaseItemHandler}>
+          &#10094;
+        </Arrow>
+      );
+    } else {
+      return <Arrow onClick={decreaseItemHandler}>&#10094;</Arrow>;
+    }
+  };
+
+  return (
+    <CheckoutItemContainer>
+      <ImageContainer>
+        <Image src={imageUrl} />
+      </ImageContainer>
+      <TextName>{name}</TextName>
+      <QuantityContainer>
+        {checkingQuantity(quantity)}
+        <TextQuantity>{quantity}</TextQuantity>
+        <Arrow onClick={increaseItemHandler}>&#10095;</Arrow>
+      </QuantityContainer>
+      <TextPrice>${price}</TextPrice>
+      <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
+  );
+};
+
+export default CheckoutItem;
